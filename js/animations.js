19ed27f2a0881ddbccb4c4b9831a88df47e08b90
Bryan Brown <bbrown98@student.gsu.edu>
2020-01-25 22:01:01 -0500

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,50 @@
+//smooth scroll to about section
+$("#about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+
+//smooth scroll to services section
+$("#service-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top
+    }, 2000);
+});
+
+//smooth scroll to services section 2
+$("#service-btn-2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top
+    }, 2000);
+});
+
+
+//smooth scroll to contact section
+$("#contact-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+
+//smooth scroll to contact section 2
+$("#contact-btn-2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+
+//smooth scroll to how we work section
+$("#work-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#how-we-work").offset().top
+    }, 2000);
+});
+
+//smooth scroll to specialists section
+$("#specialist-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#meet-the-team").offset().top
+    }, 2000);
+});
+
